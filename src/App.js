98be596394f67e570c19f39d869eb33e602727f1
Micheal Kwabena Afreh <quabenaa@gmail.com>
2020-07-01 18:53:50 +0100

    added sign-in-sign-up, sign in, form-imput and custom-button components. heade was modified to add the signin route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUp from './pages/sign-in-sign-up/sign-in-sign-up.component';
 
 import './App.css';
 
@@ -13,6 +14,7 @@ const App = () => (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
+      <Route path="/signin" component={SignInAndSignUp} />
     </Switch>
   </div>
 );
